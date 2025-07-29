891dcea6ab540b7faeaa0bc87eda7bc6fc284828
GiorgioS25 <giorgio.sangiorgi01@community.unipa.it>
2025-07-29 09:27:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -198,7 +198,7 @@ st.markdown(
     <div style="
         position: fixed;
         bottom: 10px;
-        right: 100px;
+        right: 150px;
         font-size: 0.75em;
         color: gray;
         opacity: 0.6;
